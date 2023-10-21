40919daf19d35a1b8460a8cad6f67fbd45e85e21
cristianv <c.velandiag@uniandes.edu.co>
2023-10-22 06:00:28 +1100

    agregar cv

--- app_candidate/app.py
+++ app_candidate/app.py
@@ -20,16 +20,12 @@ app.secret_key = 'dev'
 
 app.url_map.strict_slashes = False
 
-username = os.getenv('DB_USER', 'admin')
-password = os.getenv('DB_PASSWORD', 'admin')
-dbname = os.getenv('DB_NAME', 'usuarios_db')
-hostname = os.getenv('DB_HOST', 'db_usuarios')
-url_posgres = os.getenv('DATABASE_URL', 'postgresql://admin:-BcjB(IiHYp`6_=8@192.168.0.3:5432/candidates-bd')
+url_posgres = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432/candidate_db')
 
-if os.getenv('TEST_APP', "True") == 'True':
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-else:
+if not os.getenv('TEST_APP'):
     app.config["SQLALCHEMY_DATABASE_URI"] = url_posgres
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'frase-secreta'
