da1ab44f1be162c111a5a24e0c4b907f636b46f5
cristianv <c.velandiag@uniandes.edu.co>
2023-10-22 18:55:27 +1100

    fix corse

--- app_candidate/app.py
+++ app_candidate/app.py
@@ -9,7 +9,7 @@ from flask_openapi3 import OpenAPI
 
 ACTIVATE_ENDPOINTS = (('/', candidate),)
 
-info = Info(title="Candidate API", version="0.3.1")
+info = Info(title="Candidate API", version="0.3.2")
 
 app = OpenAPI(__name__,
               info=info,
@@ -39,7 +39,7 @@ with app.app_context():
 
 app_context = app.app_context()
 app_context.push()
-cors = CORS(app)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
 
 
 for url, blueprint in ACTIVATE_ENDPOINTS:
--- app_project/app.py
+++ app_project/app.py
@@ -10,7 +10,7 @@ from flask_openapi3 import OpenAPI
 ACTIVATE_ENDPOINTS = (('/', projects),)
 
 
-info = Info(title="Candidate API", version="0.2.1")
+info = Info(title="Candidate API", version="0.2.2")
 
 app = OpenAPI(__name__,
               info=info,
@@ -43,7 +43,7 @@ with app.app_context():
 
 app_context = app.app_context()
 app_context.push()
-cors = CORS(app)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
 
 
 for url, blueprint in ACTIVATE_ENDPOINTS:
