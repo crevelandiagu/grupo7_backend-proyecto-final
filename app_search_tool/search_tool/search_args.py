5ff02e1c8167c687c43534293947d23a8663e0e5
cristianv <c.velandiag@uniandes.edu.co>
2023-11-01 11:28:12 +1100

    fix: documentation api and refactor search candidate

--- app_search_tool/search_tool/search_args.py
+++ app_search_tool/search_tool/search_args.py
@@ -1,29 +1,33 @@
 from .connection_db import ConnectionDB
 
-def get_dandidate(request):
+def get_dandidate(query):
 
-    skill = request.args.get('skill')
-    year_exp = request.args.get('experienceYears')
+    skill = query.skill
+    year_exp = query.experienceYears
 
     if skill and year_exp:
-        return get_candidate_skill_year_exp(skill, year_exp)
+        return get_candidate_general(skill=skill, year_exp=year_exp)
     elif year_exp:
-        return get_candidate_year_exp(year_exp)
+        return get_candidate_general(year_exp=year_exp)
     elif skill:
-        return get_candidate_skill(skill)
+        return get_candidate_general(skill=skill)
     else:
-        pass
+        return [{}], 200
 
 
-
-def get_candidate_skill(skill):
+def get_candidate_general(skill=[], year_exp=0):
     conn_db = ConnectionDB()
-    skills = set(skill.split('-'))
-    my_query = "select * from cv_skills"
+
+    if year_exp and skill or year_exp:
+        my_query = f"select * from cv_skills where years_exp >= {year_exp}"
+    else:
+        my_query = "select * from cv_skills"
+
+    skills = set(skill.split('-')) if skill else set()
     cv_skill_records = conn_db.run_query(my_query)
 
     if not isinstance(cv_skill_records, dict):
-        len_search = len(skills)
+        len_search = len(skills) if skill else -1
         list_candidate = []
         if cv_skill_records:
             for candidate in cv_skill_records:
@@ -38,49 +42,6 @@ def get_candidate_skill(skill):
         return list_candidate, 200
     return cv_skill_records, 400
 
-def get_candidate_year_exp(year_exp):
-    conn_db = ConnectionDB()
-    year_exp = float(year_exp)
-    my_query = f"select * from cv_skills where years_exp >= {year_exp}"
-    cv_skill_records = conn_db.run_query(my_query)
-
-    if not isinstance(cv_skill_records, dict):
-        list_candidate = []
-        if cv_skill_records:
-            for candidate in cv_skill_records:
-                list_candidate.append({
-                    "name":candidate[3],
-                    "lastName": candidate[4],
-                    "skills": candidate[1],
-                    "years_exp": candidate[2],
-                    "candidateId": candidate[-1],
-                })
-        return list_candidate, 200
-    return cv_skill_records, 400
-
-
-def get_candidate_skill_year_exp(skill, year_exp):
-    conn_db = ConnectionDB()
-    year_exp = float(year_exp)
-    skills = set(skill.split('-'))
-    my_query = f"select * from cv_skills where years_exp >= {year_exp}"
-    cv_skill_records = conn_db.run_query(my_query)
-
-    if not isinstance(cv_skill_records, dict):
-        len_search = len(skills)
-        list_candidate = []
-        if cv_skill_records:
-            for candidate in cv_skill_records:
-                if len(skills & set(candidate[1].get('skills'))) >= len_search:
-                    list_candidate.append({
-                        "name":candidate[3],
-                        "lastName": candidate[4],
-                        "skills": candidate[1],
-                        "years_exp": candidate[2],
-                        "candidateId": candidate[-1],
-                    })
-        return list_candidate, 200
-    return cv_skill_records, 400
 
 '''
 f"select name.candidatte from candidatte where id.candidatte in {id_candidate}"
--- app_search_tool/search_tool/views.py
+++ app_search_tool/search_tool/views.py
@@ -8,21 +8,27 @@ from flask_openapi3 import Tag
 from flask_openapi3 import APIBlueprint
 from .search_args import get_dandidate
 from .search_cv import get_cv_candidate
-
+from .serializer import (
+    SearchQuery,
+    RESPONSE_SEARCH,
+    SearchPath,
+    RESPONSE_SEARCH_CV
+)
 
 search_tool = APIBlueprint('search_tool', __name__, url_prefix='/search-tool')
 
 search_tool_tag = Tag(name="Search Tool", description="Search candidate")
 
 
-@search_tool.get("/search", tags=[search_tool_tag])
-def get_candidate():
-    response, status = get_dandidate(request)
+@search_tool.get("/search", tags=[search_tool_tag], responses=RESPONSE_SEARCH)
+def get_candidate(query: SearchQuery):
+    response, status = get_dandidate(query)
     return response, status
 
 
-@search_tool.get("/search/cv/<int:id_candidate>", tags=[search_tool_tag])
-def get_candidate_cv():
+@search_tool.get("/search/cv/<int:id_candidate>", tags=[search_tool_tag], responses=RESPONSE_SEARCH_CV)
+def get_candidate_cv(path: SearchPath):
+    print(path)
     response, status = get_cv_candidate(request)
     return response, status
 
@@ -42,6 +48,9 @@ def root():
 
 @search_tool.get('/ping2', tags=[search_tool_health_tag])
 def ping():
+    """
+    Get ping health check
+    """
     username = os.getenv('SQLALCHEMY_DATABASE_URI', 'admin')
     return f'pong12 {username}'
 
