abc84ec08a9fc4b416740731ac1c1d0dcf774251
cristianv <c.velandiag@uniandes.edu.co>
2023-11-05 14:22:29 +1100

    app_company_employees

--- app_company_employees/company_employees/core.py
+++ app_company_employees/company_employees/core.py
@@ -44,7 +44,7 @@ def is_valid_email(email):
 
 
 def is_validate_password(password):
-  regular_expression_password = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
+  regular_expression_password = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
   pattern = re.compile(regular_expression_password)
   if re.search(pattern, password):
     return True
